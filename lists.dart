8331fb9b338911c6571b04091428ae78830b1e35
MUHAMMADYASEEN-Developer <muhammadyaseen692006@gmail.com>
2024-07-29 22:39:30 +0500

    lists

---
+++ lists.dart
@@ -0,0 +1,35 @@
+main() {
+  List<String> names = ["\"Muhammad\"", "Yaseen"];
+
+  print("\n");
+
+  print(names);
+
+  print("\n");
+
+  List<String> Days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thrusday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
+  print(Days[6]);
+
+  List<String> intro = [
+    "Name : Muhammad Yaseen",
+    "Class : 10",
+    "percentage : 76.0",
+    "Roll no : 4548",
+    "Grade : A"
+  ];
+
+  print("\n");
+
+  print(intro);
+
+  print("\n");
+}
